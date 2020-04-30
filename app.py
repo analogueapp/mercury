b889c60cb1bbde05031689c78283d11523c0a57d
Faaz Abidi <sa06195@st.habib.edu.pk>
2020-04-30 18:36:01 +0500

    Open Graph Tags Working

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from bs4 import BeautifulSoup, SoupStrainer
 import concurrent.futures
 from flask import Flask, request
 from flask import jsonify
+from open_graph import request_and_filter
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'Data Enrichment API'
+
+@app.route('/get')
+def Graph_data():
+    URL = request.args.get('url')
+    Data = request_and_filter(URL)
+    return Data
 
 if __name__ == '__main__':
     app.run(debug=True)
--- open_graph.py
+++ open_graph.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
+import time
 
-class OpenGraph:
-
-    def __init__(self, url):
-        self.url = url
-        self.tags = ['']
-
-    def get_data(self):
+def request_and_filter(url):
+    start= time.time()
+    Graph_data = {}
+    parseonly = SoupStrainer('meta')
+    head_content = BeautifulSoup(requests.get(url).text,'lxml', parse_only=parseonly).find_all('meta')
+    for meta in head_content:
+        try:
+            Graph_data[meta['property'][3:]] = meta['content']
+        except:
+            continue
+    end  = time.time()
+    Graph_data['response_time'] = '%s seconds'%(end-start)
+    return Graph_data
