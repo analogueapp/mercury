569efa5e421395a3276244b10aa7788cd67ee3ef
Faaz Abidi <sa06195@st.habib.edu.pk>
2020-05-01 05:18:04 +0500

    Updated

--- scripts/app.py
+++ scripts/app.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup, SoupStrainer
 import concurrent.futures
 from flask import Flask, request
 from flask import jsonify
+import time
 from generic_methods import open_graph
+from API_enrichment import *
 
 app = Flask(__name__)
+
+APIs =['www.goodreads.com']
 
 #landing page
 @app.route('/')
@@ -15,10 +19,18 @@ def welcome():
 #get method
 @app.route('/get')
 def Graph_data():
+    start = time.time()
     URL = request.args.get('url')
     #sending request just once
     requested = requests.get(URL).text
     Data = open_graph(requested)
+    site_name = URL.split('/')[2]
+    if site_name in APIs:
+        if site_name == 'www.goodreads.com':
+            print(site_name)
+            Data =  (fetch_goodreads(Data["ks:isbn"], Data))
+    end  = time.time()
+    Data['response_time'] = '%s seconds'%(round(end-start, 2))
     return Data
 
 if __name__ == '__main__':
--- scripts/generic_methods.py
+++ scripts/generic_methods.py
@@ -1,20 +1,17 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
-import time
 
 #getting Open graph tags data
 def open_graph(request_object):
-    start= time.time()
     Graph_data = {}
     parse_only = SoupStrainer('meta')
     head_content = BeautifulSoup(request_object,'lxml', parse_only=parse_only).find_all('meta')
     for meta in head_content:
         try:
-            Graph_data[meta['property'][3:]] = meta['content']
+            if 'og:' in meta['property']:
+                Graph_data[meta['property'][3:]] = meta['content']
         except:
             continue
-    end  = time.time()
-    Graph_data['response_time'] = '%s seconds'%(round(end-start, 2))
     return Graph_data
 
 # def simple_tags(request_object):
