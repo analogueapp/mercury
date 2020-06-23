1ef83529abdb4871981886e5e5e662cf7710418f
Faaz <57310134+FaazAbidi@users.noreply.github.com>
2020-06-23 05:00:27 +0500

    🐞Fix GET Error

--- utils/request.py
+++ utils/request.py
@@ -13,7 +13,7 @@ def handle_params(param_dict: dict) -> str:
     for v, k in zip(values, keys):
         URL += f"{k}={v}&"
 
-    URL = URL[:-1].strip("url=")
+    URL = URL[4:-1]
 
     return URL
 
@@ -21,7 +21,7 @@ def handle_params(param_dict: dict) -> str:
 def send_request(param_dict: dict):
 
     URL = handle_params(param_dict)
-
+    
     try:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
@@ -35,3 +35,4 @@ def send_request(param_dict: dict):
     except Exception as e:
         logging.error(f"Error loading page: {e}")
         return {"error": "URL failed to load"}
+
