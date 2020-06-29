4b8bb2256c72547900b2e47ab350b2ff541a65f8
Faaz <57310134+FaazAbidi@users.noreply.github.com>
2020-06-30 00:55:40 +0500

    ✨Add Search Endpoint

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ load_dotenv()
 from utils.tag_parsers import main_generic
 from utils.enrichment import enrich_test
 from utils.request import send_request, handle_params
+from utils.search import search
 
 sentry_key = os.getenv("SENTRY_KEY")
 sentry_org = os.getenv("SENTRY_ORG")
 sentry_project = os.getenv("SENTRY_PROJECT")
 
 # initializing sentry
-sentry_sdk.init(
-    dsn=f"https://{sentry_key}@{sentry_org}.ingest.sentry.io/{sentry_project}",
-    integrations=[FlaskIntegration()],
-)
+# sentry_sdk.init(
+#     dsn=f"https://{sentry_key}@{sentry_org}.ingest.sentry.io/{sentry_project}",
+#     integrations=[FlaskIntegration()],
+# )
 
 app = Flask(__name__)
 
@@ -55,6 +56,17 @@ def enrichment():
 
     return jsonify(enriched_data)
 
+
+@app.route("/search")
+def search_endpoint():
+
+    query = request.args.get("query")
+    medium = request.args.get("medium")
+    medium_id = request.args.get("id")
+
+    results = search(query,medium, medium_id)
+
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run()
