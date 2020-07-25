fa6edbd5a8de347dd0740d0bed02dde7edc9e8f2
Faaz <57310134+FaazAbidi@users.noreply.github.com>
2020-07-25 17:39:12 +0500

    ⚡Add Tests for Search Endpoint

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,7 +11,7 @@ def test_film():
     query = "batman"
     search_result = search_film(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
 
@@ -19,7 +19,7 @@ def test_tv():
     query = "love"
     search_result = tv_search(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
 
@@ -27,7 +27,7 @@ def test_book():
     query = "animal"
     search_result = search_books(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
 
@@ -35,7 +35,7 @@ def test_music():
     query = "love"
     search_result = search_music(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
 
@@ -43,7 +43,7 @@ def test_podcast():
     query = "daily"
     search_result = search_podcast(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
 
@@ -51,7 +51,7 @@ def test_web():
     query = "climate"
     search_result = search_web(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 2
 
@@ -59,7 +59,7 @@ def test_youtube():
     query = "nature"
     search_result = search_video(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 6
 
@@ -67,6 +67,6 @@ def test_games():
     query = "far cry"
     search_result = search_video_games(query)
     assert isinstance(search_result, list)
-    if len(search_result):
+    if search_result:
         assert isinstance(search_result[0], dict)
         assert len(list(search_result[0].keys())) == 5
