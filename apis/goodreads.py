f94d4a7d689c04dbf9fba345e7580051ff152f09
J03L R0J0 <rojo@analogue.com>
2021-09-11 19:45:20 -0700

    💡 Strip strings in get_text + pulication date

--- apis/goodreads.py
+++ apis/goodreads.py
@@ -48,8 +48,8 @@ def get_isbn(goodreads_id: str) -> str:
     parse_only = ["isbn", "isbn13"]
     soup_object = BeautifulSoup(requests.get(book_url).text, "html.parser")
 
-    isbn_data["isbn"] = soup_object.find("isbn").get_text()
-    isbn_data["isbn13"] = soup_object.find("isbn13").get_text()
+    isbn_data["isbn"] = soup_object.find("isbn").get_text(strip=True)
+    isbn_data["isbn13"] = soup_object.find("isbn13").get_text(strip=True)
 
     return isbn_data
 
@@ -59,12 +59,12 @@ def enrich_author(goodreads_id):
     author_url = f"{goodreads_author_url}?id={goodreads_id}&key={goodreads_key}"
     author_soup = BeautifulSoup(requests.get(author_url).text, "html.parser")
 
-    author_data["about"] = author_soup.find("about").get_text()
-    author_data["born_at"] = author_soup.find("born_at").get_text()
-    author_data["died_at"] = author_soup.find("died_at").get_text()
-    author_data["hometown"] = author_soup.find("hometown").get_text()
-    author_data["large_image_url"] = author_soup.find("large_image_url").get_text()
-    author_data["name"] = author_soup.find("name").get_text()
+    author_data["about"] = author_soup.find("about").get_text(strip=True)
+    author_data["born_at"] = author_soup.find("born_at").get_text(strip=True)
+    author_data["died_at"] = author_soup.find("died_at").get_text(strip=True)
+    author_data["hometown"] = author_soup.find("hometown").get_text(strip=True)
+    author_data["large_image_url"] = author_soup.find("large_image_url").get_text(strip=True)
+    author_data["name"] = author_soup.find("name").get_text(strip=True)
 
     return author_data
 
@@ -79,43 +79,43 @@ def fetch_goodreads(url: str) -> Dict:
     print(soup_object)
 
     authors_data = soup_object.find("authors")
-    isbn = soup_object.find("isbn").get_text()
-    original_publication_year = soup_object.find("original_publication_year").get_text()
+    isbn = soup_object.find("isbn").get_text(strip=True)
+    original_publication_year = soup_object.find("original_publication_year").get_text(strip=True)
     original_publication_month = soup_object.find(
         "original_publication_month"
-    ).get_text()
-    original_publication_day = soup_object.find("original_publication_day").get_text()
-    publication_year = soup_object.find("publication_year").get_text()
-    publication_month = soup_object.find("publication_month").get_text()
-    publication_day = soup_object.find("publication_day").get_text()
+    ).get_text(strip=True)
+    original_publication_day = soup_object.find("original_publication_day").get_text(strip=True)
+    publication_year = soup_object.find("publication_year").get_text(strip=True)
+    publication_month = soup_object.find("publication_month").get_text(strip=True)
+    publication_day = soup_object.find("publication_day").get_text(strip=True)
 
-    api_data["asin"] = soup_object.find("asin").get_text()
+    api_data["asin"] = soup_object.find("asin").get_text(strip=True)
     api_data["authors"] = [enrich_author(id.text) for id in authors_data.find_all("id")]
-    api_data["description"] = soup_object.find("description").get_text()
-    api_data["edition_information"] = soup_object.find("edition_information").get_text()
+    api_data["description"] = soup_object.find("description").get_text(strip=True)
+    api_data["edition_information"] = soup_object.find("edition_information").get_text(strip=True)
     api_data["form"] = "text"
     api_data["goodreads_id"] = gid
     api_data["image_url"] = get_image_url(
-        isbn, soup_object.find("image_url").get_text()
+        isbn, soup_object.find("image_url").get_text(strip=True)
     )
     api_data["isbn"] = isbn
-    api_data["isbn13"] = soup_object.find("isbn13").get_text()
-    api_data["kindle_asin"] = soup_object.find("kindle_asin").get_text()
-    api_data["language"] = soup_object.find("language_code").get_text()
+    api_data["isbn13"] = soup_object.find("isbn13").get_text(strip=True)
+    api_data["kindle_asin"] = soup_object.find("kindle_asin").get_text(strip=True)
+    api_data["language"] = soup_object.find("language_code").get_text(strip=True)
     api_data["medium"] = "book"
     api_data["origin"] = "amazon.com"
     api_data["origin_url"] = f"https://www.amazon.com/dp/{isbn}"
     api_data[
         "original_publication_date"
-    ] = f"{original_publication_year}/{original_publication_month}/{original_publication_day}"
+    ] = f"{original_publication_year}/{original_publication_month}/{original_publication_day}"  if original_publication_year else ''
     api_data["popular_shelves"] = [
         shelf["name"] for shelf in soup_object.find_all("shelf")
     ]
-    api_data["publisher"] = soup_object.find("publisher").get_text()
+    api_data["publisher"] = soup_object.find("publisher").get_text(strip=True)
     api_data[
         "publication_date"
-    ] = f"{publication_year}/{publication_month}/{publication_day}"
-    api_data["title"] = soup_object.find("title").get_text()
+    ] = f"{publication_year}/{publication_month}/{publication_day}" if publication_year else ''
+    api_data["title"] = soup_object.find("title").get_text(strip=True)
 
     return api_data
 
