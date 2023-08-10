bf28145c48e5f67c1c935c9fd84518265ce00c51
J03L R0J0 <rojo@analogue.com>
2023-08-10 16:35:52 -0700

    ⚙️ Config vscode settings for auto formatting

--- utils/get_main.py
+++ utils/get_main.py
@@ -2,8 +2,8 @@ from utils.tag_parsers import main_generic
 from utils.get_facebook import get_facebook_API
 from typing import Dict
 
-def get_main(requests_object ,url) -> dict:
 
+def get_main(requests_object, url) -> dict:
     response = get_facebook_API(url)
 
     if None in [response[x] for x in response.keys()]:
@@ -12,7 +12,7 @@ def get_main(requests_object ,url) -> dict:
         for key in response.keys():
             if response[key] is None:
                 response[key] = tag_parser_response[key]
-        
+
         return response
 
     return response
