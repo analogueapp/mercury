5c726989d2ecce919977f0b94df276e8ada6040d
J03L R0J0 <rojo@analogue.com>
2023-08-10 17:31:39 -0700

    🐞 Fix google books search results error

--- search/book.py
+++ search/book.py
@@ -4,32 +4,37 @@ from constants import google_api_url
 import json
 import os
 
-google_key = os.getenv('GOOGLE_KEY')
+google_key = os.getenv("GOOGLE_KEY")
+
 
 def search_isbn(isbn: str) -> str:
-    url = f'{google_api_url}?q=isbn:{isbn}&key={google_key}'
+    url = f"{google_api_url}?q=isbn:{isbn}&key={google_key}"
     resp = requests.get(url)
     data = resp.json()
     volume_url = data["items"][0]["selfLink"]
 
     return volume_url
 
-def search_books(query):    
-    results = []    
-    api_url = f'{google_api_url}?q=intitle:{query}&key={google_key}'    
-    resp = requests.get(api_url)
-    data = resp.json()            
 
-    for result in data["items"]:        
-        single_result = {}        
-        
-        single_result['title'] = result["volumeInfo"]["title"]
-        single_result['image'] = result["volumeInfo"]["imageLinks"]["thumbnail"]
-        single_result['creators'] = result["volumeInfo"]["authors"]
-        single_result['url'] = result["selfLink"]
-        single_result['medium'] = 'book'
+def search_books(query):
+    results = []
+    api_url = f"{google_api_url}?q=intitle:{query}&key={google_key}"
+    resp = requests.get(api_url)
+    data = resp.json()
+
+    for result in data["items"]:
+        single_result = {}
+
+        # Check if key exists before accessing
+        single_result["title"] = result["volumeInfo"].get("title", "Unknown Title")
+        single_result["image"] = result["volumeInfo"]["imageLinks"].get(
+            "thumbnail", None
+        )
+        single_result["creators"] = result["volumeInfo"].get("authors", [])
+        single_result["url"] = result.get("selfLink", "")
+        single_result["medium"] = "book"
 
         results.append(single_result)
         del single_result
-    
+
     return results
