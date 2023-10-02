957f5edbc99a8a634e0c53aaa8412215667f5bcb
Spencer Small <spencerjsmall>
2023-10-02 09:38:03 -0700

    minor bug fix

--- apis/excerpts.py
+++ apis/excerpts.py
@@ -7,8 +7,8 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
 
 def get_excerpt(work, creatorList):  
-    prompt_text = (f"Generate a one sentence excerpt of {work}, by {', '.join(creatorList)}. If you are unfamiliar with the work, respond only with 'Unknown'")
     completion = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=prompt_text, max_tokens=3000, temperature=0.6, n=1)    
+    prompt_text = (f"Generate a one sentence excerpt of {work}, by {', '.join(creatorList)}.")
             
     excerpt = completion.choices[0].text.strip().strip('"')
      
