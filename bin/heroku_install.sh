b53cdfd2205979fdc680871da9b41c8508f4141f
Spencer Small <spencerjsmall>
2023-10-06 11:09:00 -0700

    move torch wheel install to script

--- bin/heroku_install.sh
+++ bin/heroku_install.sh
@@ -3,5 +3,7 @@
 # Install main dependencies
 pip3 install -r requirements.txt
 
+pip3 install http://download.pytorch.org/whl/cpu/torch-2.1.0%2Bcpu-cp311-cp311-linux_x86_64.whl
+
 # Install no-deps requirements
 pip3 install --no-deps sentence-transformers
