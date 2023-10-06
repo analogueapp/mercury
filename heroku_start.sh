a3b715c0194491c7a8e051c75f1d5aafc77f8a85
Spencer Small <spencerjsmall>
2023-10-06 12:39:16 -0700

    install sentence-trasnformers after requirements

--- heroku_start.sh
+++ heroku_start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+pip3 install -r requirements.txt
+
 # Install sentence-transformers without dependencies
 pip3 install --no-deps sentence-transformers
 
 # Start the application
-gunicorn app:app --timeout 25 --log-level info --graceful-timeout 30
+gunicorn app:app --timeout 25 --log-level info
